5c144dab5f5b1c061fd69341b45007484111739d
Teo Welton <teowelton@gmail.com>
2024-03-11 13:39:57 -0600

    Prettier Code Formatting

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,32 +1,40 @@
+-- Local Vars --
+local formatters = require "lvim.lsp.null-ls.formatters"
+
 -- PLUGINS --
 lvim.plugins = {
-  {"catppuccin/nvim"}, -- Catppuccin theme
-  {"ghillb/cybu.nvim"}, -- Cybu Buffer Switcher
-  {"andweeb/presence.nvim"}, -- Discord Presence for Neovim
-  {"fadein/vim-figlet"}, -- Figlet generation in neovim
-  {"github/copilot.vim"}, -- GitHub Copilot
-  {"iamcco/markdown-preview.nvim", -- Preview Markdown files
-  	ft={"markdown"},
-	  build = function() vim.fn["mkdp#util#install"]() end,
+  { "catppuccin/nvim" },            -- Catppuccin theme
+  { "ghillb/cybu.nvim" },           -- Cybu Buffer Switcher
+  { "andweeb/presence.nvim" },      -- Discord Presence for Neovim
+  { "fadein/vim-figlet" },          -- Figlet generation in neovim
+  { "github/copilot.vim" },         -- GitHub Copilot
+  {
+    "iamcco/markdown-preview.nvim", -- Preview Markdown files
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
   },
 }
 
 -- Themeing & Fonts --
-lvim.colorscheme = "catppuccin" -- Set colorscheme (catppuccin best :D)
+lvim.colorscheme = "catppuccin"        -- Set colorscheme (catppuccin best :D)
 vim.o.guifont = "VictorMono Nerd Font" -- Set Font if using neovide gui
-lvim.transparent_window = true -- Make the window transparent
+lvim.transparent_window = true         -- Make the window transparent
 
 -- Markdown Preview Settings --
-vim.g.mkdp_auto_start = 1 -- Make it so that when a markdown file is open, it will launch the preview
+vim.g.mkdp_auto_start = 1  -- Make it so that when a markdown file is open, it will launch the preview
 vim.g.mkdp_theme = 'light' -- Make it so that the preview defaults to light mode
 
 -- Start LaTeX --
-require("lspconfig").ltex.setup{}
+require("lspconfig").ltex.setup {}
+
+-- Formatting --
+lvim.format_on_save.enabled = true
+formatters.setup { { name = "prettier" }, }
 
 -- KEYBINDS --
-lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>" -- Press 'X' to close buffer
-lvim.keys.normal_mode["<S-h>"] = ":CybuPrev<CR>" -- Switch Buffer w/ Cybu (previous)
-lvim.keys.normal_mode["<S-l>"] = ":CybuNext<CR>" -- Switch Buffer w/ Cybu (next)
-lvim.keys.normal_mode["|"] = ":vsplit<CR>" -- Split Vertically (Without nvim tree)
-lvim.keys.normal_mode["-"] = ":split<CR>" -- Split horizontally (Without nvm tree)
+lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"      -- Press 'X' to close buffer
+lvim.keys.normal_mode["<S-h>"] = ":CybuPrev<CR>"        -- Switch Buffer w/ Cybu (previous)
+lvim.keys.normal_mode["<S-l>"] = ":CybuNext<CR>"        -- Switch Buffer w/ Cybu (next)
+lvim.keys.normal_mode["|"] = ":vsplit<CR>"              -- Split Vertically (Without nvim tree)
+lvim.keys.normal_mode["-"] = ":split<CR>"               -- Split horizontally (Without nvm tree)
 lvim.keys.normal_mode["<C-p>"] = ":MarkdownPreview<CR>" -- Start markdown preview with Ctrl _ P
