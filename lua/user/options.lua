200e411a6e8cc7d7e60daf0050f2789479971078
Teo Welton <teowelton@gmail.com>
2024-05-17 19:57:28 -0600

    Keybinds and option changes

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,33 +1,24 @@
--- Local Vars --
+------ Local Vars ------
 local formatters = require "lvim.lsp.null-ls.formatters"
 
--- PLUGINS --
+------ PLUGINS ------
 lvim.plugins = {
   { "catppuccin/nvim" },                     -- Catppuccin theme
   { "ghillb/cybu.nvim" },                    -- Cybu Buffer Switcher
   { "andweeb/presence.nvim" },               -- Discord Presence for Neovim
   { "fadein/vim-figlet" },                   -- Figlet generation in neovim
   { 'wakatime/vim-wakatime', lazy = false }, -- Wakatime
-  {
-    "iamcco/markdown-preview.nvim",          -- Preview Markdown files
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-  },
 }
 
--- Themeing & Fonts --
+------ Themeing & Fonts ------
 lvim.colorscheme = "catppuccin"        -- Set colorscheme (catppuccin best :D)
 vim.o.guifont = "VictorMono Nerd Font" -- Set Font if using neovide gui
 lvim.transparent_window = true         -- Make the window transparent
 
--- Markdown Preview Settings --
-vim.g.mkdp_auto_start = 1  -- Make it so that when a markdown file is open, it will launch the preview
-vim.g.mkdp_theme = 'light' -- Make it so that the preview defaults to light mode
-
--- Start LaTeX --
+----- Start LaTeX -----
 require("lspconfig").ltex.setup {}
 
--- Formatting --
+----- Formatting -----
 lvim.format_on_save.enabled = true
 formatters.setup {
   {
@@ -44,14 +35,21 @@ formatters.setup {
   }
 }
 
--- KEYBINDS --
-lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"      -- Press 'X' to close buffer
-lvim.keys.normal_mode["<S-h>"] = ":CybuPrev<CR>"        -- Switch Buffer w/ Cybu (previous)
-lvim.keys.normal_mode["<S-l>"] = ":CybuNext<CR>"        -- Switch Buffer w/ Cybu (next)
-lvim.keys.normal_mode["|"] = ":vsplit<CR>"              -- Split Vertically (Without nvim tree)
-lvim.keys.normal_mode["-"] = ":split<CR>"               -- Split horizontally (Without nvm tree)
-lvim.keys.normal_mode["<C-p>"] = ":MarkdownPreview<CR>" -- Start markdown preview with Ctrl _ P
-lvim.keys.normal_mode["<Left>"] = "<Nop>"               -- Remove Left Arrow (USE HJKL)
-lvim.keys.normal_mode["<Right>"] = "<Nop>"              -- Remove Left Arrow (USE HJKL)
-lvim.keys.normal_mode["<Up>"] = "<Nop>"                 -- Remove Left Arrow (USE HJKL)
-lvim.keys.normal_mode["<Down>"] = "<Nop>"               -- Remove Left Arrow (USE HJKL)
+----- KEYBINDS -----
+-- General --
+lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>" -- Press 'X' to close buffer
+lvim.keys.normal_mode["<S-h>"] = ":CybuPrev<CR>"   -- Switch Buffer w/ Cybu (previous)
+lvim.keys.normal_mode["<S-l>"] = ":CybuNext<CR>"   -- Switch Buffer w/ Cybu (next)
+lvim.keys.normal_mode["|"] = ":vsplit<CR>"         -- Split Vertically (Without nvim tree)
+lvim.keys.normal_mode["-"] = ":split<CR>"          -- Split horizontally (Without nvm tree)
+
+-- Remove Arrow Keys --
+lvim.keys.normal_mode["<Left>"] = "<Nop>"  -- Remove Left Arrow (Normal Mode)
+lvim.keys.normal_mode["<Right>"] = "<Nop>" -- Remove Right Arrow (Normal Mode)
+lvim.keys.normal_mode["<Up>"] = "<Nop>"    -- Remove Up Arrow (Normal Mode)
+lvim.keys.normal_mode["<Down>"] = "<Nop>"  -- Remove Down Arrow (Normal Mode)
+
+lvim.keys.visual_mode["<Left>"] = "<Nop>"  -- Remove Left Arrow (Visual Mode)
+lvim.keys.visual_mode["<Right>"] = "<Nop>" -- Remove Right Arrow (Visual Mode)
+lvim.keys.visual_mode["<Up>"] = "<Nop>"    -- Remove Up Arrow (Visual Mode)
+lvim.keys.visual_mode["<Down>"] = "<Nop>"  -- Remove Down Arrow (Visual Mode)
